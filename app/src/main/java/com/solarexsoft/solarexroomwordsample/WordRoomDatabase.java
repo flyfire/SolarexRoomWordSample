9f284425efe4c41e957483b20225bc3fd5e4559f
Solarex <rh.hou.work@gmail.com>
2020-04-29 20:08:44 +0800

    java

--- app/src/main/java/com/solarexsoft/solarexroomwordsample/WordRoomDatabase.java
+++ app/src/main/java/com/solarexsoft/solarexroomwordsample/WordRoomDatabase.java
@@ -2,9 +2,11 @@ package com.solarexsoft.solarexroomwordsample;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -27,10 +29,26 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                 if (INSTANCE == null) {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             WordRoomDatabase.class, "word_database")
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
         return INSTANCE;
     }
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback(){
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+            databaseWriteExecutor.execute(() -> {
+                WordDao dao = INSTANCE.wordDao();
+                dao.deleteAll();
+                Word word = new Word("hello");
+                dao.insert(word);
+                word = new Word("world");
+                dao.insert(word);
+            });
+        }
+    };
 }

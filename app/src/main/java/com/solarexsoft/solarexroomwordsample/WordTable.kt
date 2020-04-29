9df126d04fdf868f4d9e42599c61f9f94999c646
Solarex <rh.hou.work@gmail.com>
2020-04-29 16:04:51 +0800

    dao

--- app/src/main/java/com/solarexsoft/solarexroomwordsample/WordTable.kt
+++ app/src/main/java/com/solarexsoft/solarexroomwordsample/WordTable.kt
@@ -1,8 +1,6 @@
 package com.solarexsoft.solarexroomwordsample
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room.*
 
 /**
  * Created by Solarex on 2020/4/29/3:53 PM
@@ -11,3 +9,15 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "word_table")
 class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
+
+@Dao
+interface WordDao {
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    fun getAlphabetizedWords(): List<Word>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
